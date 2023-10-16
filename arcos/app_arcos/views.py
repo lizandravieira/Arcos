d2c5baaf9ed3c582bb747627c268387fdfef9767
Pedrolira16 <pwls@cesar.school>
2023-10-16 19:50:21 -0300

    auth routes

--- arcos/app_arcos/views.py
+++ arcos/app_arcos/views.py
@@ -1,7 +1,52 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, "index.html")
 
 def login(request):
-    return render(request, "auth/login.html")
+    if request.method == "GET":
+        return render(request, "auth/login.html")
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login_django(request, user)
+
+            return HttpResponse("autenticado")
+        else:
+            return HttpResponse("Email ou senha invalidos")
+
+@login_required(login_url="/login/")
+def plataforma(request):
+    if request.user.is_authenticated:
+        return HttpResponse("Plataforma")
+    
+
+
+def register(request):
+    if request.method== "GET":
+        return render(request,"auth/register.html")
+    else:
+        username=request.POST.get("username")
+        email =request.POST.get("email")
+        password =request.POST.get("password")
+        
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse("Já existe um usuário cadastrado")
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        return HttpResponse("usuário cadastrado com sucesso")
+    
+
+        
--- arcos/arcos/urls.py
+++ arcos/arcos/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib import admin
 from django.urls import path
 from app_arcos import views
 
@@ -6,6 +6,7 @@ from app_arcos import views
 urlpatterns = [
   path("", views.index, name="index"),
   path("login/", views.login, name="login"),
-  # path("logout/", views.logout_view, name="logout"),
-  # path("register/", views.register, name="register"),
+  path("register/",views.register, name="register"),
+  path("admin/", admin.site.urls),
+  path("plataforma",views.plataforma, name="plataforma"),
 ]
